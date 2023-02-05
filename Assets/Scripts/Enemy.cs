724222d0422a015c77996af12babd107f9fef4ad
Galen Molk <76459193+galenmolk@users.noreply.github.com>
2023-02-05 00:13:13 -0500

    broken enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float threshold;
+    public float speed;
+
+    public Rigidbody2D rb;
+    public Rigidbody2D playerRb;
+
+    private void FixedUpdate() 
+    {
+        Vector2 direction = playerRb.position - rb.position;
+        rb.velocity = direction.normalized * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BramblyMead;
+
+public class Player : Singleton<Player>
+{
+    public Vector2 RBPos => rb.position;
+
+    public Rigidbody2D rb;
+}
